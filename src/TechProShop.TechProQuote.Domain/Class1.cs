bbf4285e3f41cfbe413fff5441ec5ac766228dca
Cédric P. BONNOT <cedric.bonnot@intelleotech.co.uk>
2026-05-28 16:02:55 +0200

    Ajout entité Client minimale

--- src/TechProShop.TechProQuote.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TechProShop.TechProQuote.Domain;
-
-public class Class1
-{
-
-}
---
+++ src/TechProShop.TechProQuote.Domain/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TechProShop.TechProQuote.Domain.Entities
+{
+    public class Client
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Nom { get; init; }
+        public required string Email { get; init; }
+        public DateTime DateCreation { get; init; } = DateTime.UtcNow;
+
+    }
+}
