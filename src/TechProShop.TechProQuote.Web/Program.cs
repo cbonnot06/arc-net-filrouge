549d670ccca42055b454e4c4fe55b880318f6f6d
Cédric P. BONNOT <cedric.bonnot@intelleotech.co.uk>
2026-05-29 11:44:19 +0200

    Support Minimal Api

--- src/TechProShop.TechProQuote.Web/Program.cs
+++ src/TechProShop.TechProQuote.Web/Program.cs
@@ -1,12 +1,27 @@
+using System.Reflection.Metadata.Ecma335;
+using TechProShop.TechProQuote.Domain.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.AddOpen
+
+
+
+
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.MapControllerRoute(
     name: "default",
@@ -15,5 +30,31 @@ app.MapControllerRoute(
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+// /api/clients
+
+var apiClients = app.MapGroup("/api/clients");
+
+apiClients.MapGet("/", () => new[]
+        {
+                new Client { Nom = "Alice Martin", Email = "alice@ex.com" },
+                new Client { Nom = "Bob Durand", Email = "bob@ex.com" }
+         }
+);
+
+apiClients.MapGet("/{id:guid}", (Guid id) =>
+{
+    return Results.Ok
+    (
+    new Client
+    {
+        Id = id,
+        Nom = "Test",
+        Email = "test@ex.com"
+    });
+});
+
+apiClients.MapPost("/", (Client client) =>
+    Results.Created($"/api.clients/{client.Id}", client));
 
 app.Run();
