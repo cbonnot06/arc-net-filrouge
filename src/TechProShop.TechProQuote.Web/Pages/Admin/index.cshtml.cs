40c7b3006699fd2ba060670cd72e561d8fd241a4
Cédric P. BONNOT <cedric.bonnot@intelleotech.co.uk>
2026-05-29 10:54:21 +0200

    Support Razor Pages

---
+++ src/TechProShop.TechProQuote.Web/Pages/Admin/index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechProShop.TechProQuote.Web.Pages.Admin
+{
+    public class InventaireModel : PageModel
+    {
+        public void OnGet()
+        {
+            string test = "";
+        }
+    }
+}
--- src/TechProShop.TechProQuote.Web/Program.cs
+++ src/TechProShop.TechProQuote.Web/Program.cs
@@ -1,15 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
 app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
 
 app.Run();
