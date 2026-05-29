373317ddd14bd8c2c0bfcf43df28af93abb3bc22
Cédric P. BONNOT <cedric.bonnot@intelleotech.co.uk>
2026-05-29 09:57:56 +0200

    ASP.NET Core MVC scaffold avec Home et Clients

---
+++ src/TechProShop.TechProQuote.Web/Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechProShop.TechProQuote.Domain.Entities;
+
+namespace TechProShop.TechProQuote.Web.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var clients = new List<Client> 
+            { 
+                new Client { Nom = "Alice Martin", Email = "alice@ex.com" }, 
+                new Client { Nom = "Bob Durand", Email = "bob@ex.com" } 
+            }; 
+
+            return View(clients);
+        }
+    }
+}
